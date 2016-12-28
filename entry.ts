48a4a264aaac1555b7da55201fd836412746ab3d
walter torricos <walticotc@gmail.com>
2016-12-28 17:39:22 -0400

    Add dev command to watch and recompile the code and relaunch the server

--- entry.ts
+++ entry.ts
@@ -1,5 +1,6 @@
 import { Server } from './server/app';
 
+const port = 3000;
 const app = (new Server()).app;
 
-app.listen(3000, () => console.log('listen on port 3000'));
+app.listen(port, () => console.log('listen on port: ' + port));
